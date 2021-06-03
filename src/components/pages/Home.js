4a937969d53a9cc404384e6c5a66f32ac220d2ef
errne <upapaus@gmail.com>
2021-06-03 18:13:38 +0100

    hompepage

---
+++ src/components/pages/Home.js
@@ -0,0 +1,10 @@
+import React,{Fragment} from 'react';
+
+const Home=()=>(
+  <Fragment>
+    <div className="row my-3">
+        <p>Lets remember Javascript</p>
+    </div>
+  </Fragment>
+)
+export default Home;
