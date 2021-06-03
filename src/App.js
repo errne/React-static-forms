2a3bb6eb92e69b4dd9f33418aa3ba5a901683e90
errne <upapaus@gmail.com>
2021-06-03 18:37:50 +0100

    making of homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router,Route,Link} from 'react-router-dom';
 import logo from './logo.svg';
+import Home from './components/pages/Home.js';
 import './App.css';
 
-function App() {
+class App extends Component {
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid px-0">
+      <Router>
+        <>
+        <div className="content-area mx-5">
+          <Route exact path="/" component={Home} />
+
+        </div>
+        </>
+      </Router>
     </div>
   );
+}
+
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Lets remember Javascript.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,7 +3,7 @@ import React,{Fragment} from 'react';
 const Home=()=>(
   <Fragment>
     <div className="row my-3">
-        <p>Lets remember Javascript</p>
+        <p>Lets remember Javascripts</p>
     </div>
   </Fragment>
 )
