27c6ea8deddd0d2813a8ccecd1b633665843d2c9
errne <upapaus@gmail.com>
2021-07-04 19:18:35 +0100

    testing circleci

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -13,6 +13,7 @@ import React,{Fragment, Component} from 'react';
 import {Link} from 'react-router-dom';
 import Request from '../../helper/Request.js'
 import {url} from '../../helper/AWSRoute.js';
+// let url2 = process.env.AWSURL
 
 import CustomerList from '../bits/CustomerList.js';
 
